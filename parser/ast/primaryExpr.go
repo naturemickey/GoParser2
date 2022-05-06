4bcacd8793463b6f9e08c3002a2610102b5fa58b
周游 <mickey_zhouyou@yeah.net>
2022-05-06 10:58:33 +0800

    complete some visitor

--- parser/ast/primaryExpr.go
+++ parser/ast/primaryExpr.go
@@ -14,8 +14,98 @@ type PrimaryExpr struct {
 	//		| typeAssertion
 	//		| arguments
 	//	);
+
+	operand    *Operand
+	conversion *Conversion
+	methodExpr *MethodExpr
+
+	primaryExpr *PrimaryExpr // 这里有一个首递归
+	suffix      *PrimaryExprSuffix
+}
+
+type PrimaryExprSuffix struct {
+	dot, identifier *lex.Token
+	index           *Index
+	slice           *Slice
+	typeAssertion   *TypeAssertion
+	arguments       *Arguments
 }
 
 func VisitPrimaryExpr(lexer *lex.Lexer) *PrimaryExpr {
-	panic("todo")
+	clone := lexer.Clone()
+
+	var primaryExpr *PrimaryExpr
+	operand := VisitOperand(lexer)
+	if operand != nil {
+		primaryExpr = &PrimaryExpr{operand: operand}
+	} else {
+		conversion := VisitConversion(lexer)
+		if conversion != nil {
+			primaryExpr = &PrimaryExpr{conversion: conversion}
+		} else {
+			methodExpr := VisitMethodExpr(lexer)
+			if conversion != nil {
+				primaryExpr = &PrimaryExpr{methodExpr: methodExpr}
+			}
+		}
+	}
+	if primaryExpr == nil {
+		lexer.Recover(clone)
+		return nil
+	}
+
+	for { // 处理首递归
+		suffix := _tryVisitSuffix(lexer)
+		if suffix == nil {
+			break
+		} else {
+			primaryExpr = &PrimaryExpr{primaryExpr: primaryExpr, suffix: suffix}
+		}
+	}
+	return primaryExpr
+}
+
+func _tryVisitSuffix(lexer *lex.Lexer) *PrimaryExprSuffix {
+	//	(DOT IDENTIFIER)
+	//	| index
+	//	| slice
+	//	| typeAssertion
+	//	| arguments
+	clone := lexer.Clone()
+
+	dot := lexer.LA()
+	if dot.Type_() == lex.GoLexerDOT {
+		lexer.Pop() // dot
+		identifier := lexer.LA()
+		if identifier.Type_() == lex.GoLexerIDENTIFIER {
+			lexer.Pop() // identifier
+			return &PrimaryExprSuffix{dot: dot, identifier: identifier}
+		} else {
+			lexer.Recover(clone)
+			dot = nil
+		}
+	}
+
+	index := VisitIndex(lexer)
+	if index != nil {
+		return &PrimaryExprSuffix{index: index}
+	}
+
+	slice := VisitSlice(lexer)
+	if slice != nil {
+		return &PrimaryExprSuffix{slice: slice}
+	}
+
+	typeAssertion := VisitTypeAssertion(lexer)
+	if typeAssertion != nil {
+		return &PrimaryExprSuffix{typeAssertion: typeAssertion}
+	}
+
+	arguments := VisitArguments(lexer)
+	if arguments != nil {
+		return &PrimaryExprSuffix{arguments: arguments}
+	}
+
+	lexer.Recover(clone)
+	return nil
 }
